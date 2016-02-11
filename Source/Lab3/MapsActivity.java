0c07363153b3dff4ee6f7836b65fda3791063f60
MounikaDandamudi <mounikadandamudi@gmail.com>
2016-02-10 23:28:46 -0600

    commit

--- Source/Lab3/MapsActivity.java
+++ Source/Lab3/MapsActivity.java
@@ -1,107 +1,111 @@
 package com.example.mounika.cameraaadlocation;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.widget.EditText;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-import java.io.IOException;
 import java.util.List;
 
-public class MapsActivity extends FragmentActivity {
 
-    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private GoogleMap mMap;
+    public Geocoder geocoder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        setUpMapIfNeeded();
+//        getActionBar().setTitle("Map location");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
-    protected void onResume() {
-        super.onResume();
-        setUpMapIfNeeded();
-    }
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        geocoder = new Geocoder(this);
+        StringBuilder userAddress = new StringBuilder();
+        // Add a marker in Sydney and move the camera
+        LocationManager userCurrentLocation = (LocationManager) this
+                .getSystemService(Context.LOCATION_SERVICE);
+        LocationListener userCurrentLocationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
 
-
-    public void onSearch(View view) {
-        EditText location_tf = (EditText) findViewById(R.id.TFaddress);
-        String location = location_tf.getText().toString();
-        List<Address> addressList = null;
-        if (location != null || !location.equals("")) {
-            Geocoder geocoder = new Geocoder(this);
-            try {
-                addressList = geocoder.getFromLocationName(location, 1);
-
-
-            } catch (IOException e) {
-                e.printStackTrace();
             }
 
-            Address address = addressList.get(0);
-            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
-            mMap.addMarker(new MarkerOptions().position(latLng).title("Marker"));
-            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
 
-        }
-    }
-
-    public void onZoom(View view) {
-        if (view.getId() == R.id.Bzoomin) {
-            mMap.animateCamera(CameraUpdateFactory.zoomIn());
-        }
-        if (view.getId() == R.id.Bzoomout) {
-            mMap.animateCamera(CameraUpdateFactory.zoomOut());
-        }
-    }
-
-    public void changeType(View view) {
-        if (mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
-            mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
-        } else
-            mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-    }
-
-    private void setUpMapIfNeeded() {
-        // Do a null check to confirm that we have not already instantiated the map.
-        if (mMap == null) {
-            // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
-            // Check if we were successful in obtaining the map.
-            if (mMap != null) {
-                setUpMap();
             }
-        }
-    }
 
-    private void setUpMap() {
-        // mMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+        LatLng userCurrentLocationCorodinates = null;
+        double latitute = 0, longitude = 0;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat
+                .checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            //show message or ask permissions from the user.
             return;
         }
-        mMap.setMyLocationEnabled(true);
-        //mMap.addMarker(new MarkerOptions().position(new LatLng().title("Marker"));
+        //Getting the current location of the user.
+        userCurrentLocation.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                0, 0, userCurrentLocationListener);
+        latitute = userCurrentLocation
+                .getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                .getLatitude();
+        longitude = userCurrentLocation
+                .getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                .getLongitude();
+        userCurrentLocationCorodinates = new LatLng(latitute,longitude);
+        //Getting the address of the user based on latitude and longitude.
+        try {
+            List<Address> addresses = geocoder.getFromLocation(latitute, longitude, 1);
+            Address address = addresses.get(0);
+            userAddress =  new StringBuilder();
+            for (int i = 0; i < address.getMaxAddressLineIndex(); i++) {
+                userAddress.append(address.getAddressLine(i)).append("\t");
+            }
+            userAddress.append(address.getCountryName()).append("\t");
+
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+        //Setting our image as the marker icon.
+        mMap.addMarker(new MarkerOptions().position(userCurrentLocationCorodinates)
+                .title("Your current address.").snippet(userAddress.toString())
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+        //Setting the zoom level of the map.
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userCurrentLocationCorodinates, 7));
 
 
     }
